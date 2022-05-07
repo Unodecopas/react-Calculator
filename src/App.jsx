4df7b935d230546dc67f08f0e6860a5c497d07b7
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-05-07 11:57:58 +0200

    feat: component - ButtonBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Wrapper from "./components/Wrapper"
 import Screen from "./components/Screen"
+import ButtonBox from "./components/ButtonBox"
 
 const App = () => {
     return (
         <div className="App">
             <Wrapper>
                 <Screen value='0' />
+                <ButtonBox>
+                    botones por aqui
+                </ButtonBox>
             </Wrapper>
         </div>
     )
